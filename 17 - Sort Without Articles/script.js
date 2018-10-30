37e9c2e060e577c75682bd746b3a37352f466103
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-10-30 08:28:10 +0600

    Seventeenth challenge implementation (Sorting Band names without articles).

--- 17 - Sort Without Articles/script.js
+++ 17 - Sort Without Articles/script.js
@@ -13,3 +13,13 @@ const bands = [
     'Anywhere But Here',
     'An Old Dog'
 ];
+
+const sortedBands = bands.sort(
+    (a, b) => strip(a) > strip(b) ? 1 : -1
+);
+
+document.querySelector('#bands').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
+
+function strip(bandName) {
+    return bandName.replace(/^(a |the |an )/i, '').trim();
+}
