1be1eca53a20fa1a2592d6e719067e6b3186a0a4
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-11-01 07:57:14 +0600

    Nineteenth challenge implementation (Unreal Webcam fun).

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,110 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            //  DEPRECIATION :
+            //       The following has been depreceated by major browsers as of Chrome and Firefox.
+            //       video.src = window.URL.createObjectURL(localMediaStream);
+            //       Please refer to these:
+            //       Depreceated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+            //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(error => {
+            console.error('OH NO!!!', error);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        // Take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // Mess with them
+        // pixels = redEffect(pixels);
+
+        pixels = rgbSplit(pixels);
+
+        // pixels = greenScreen(pixels);
+        // ctx.globalAlpha = 0.8;
+
+        // Put them back
+        ctx.putImageData(pixels, 0, 0);
+    }, 16)
+}
+
+function takePhoto() {
+    // Played the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    // Take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    // link.textContent = 'Download Image';
+    link.innerHTML = `<img src="${data}" alt="Handsome">`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+= 4) {
+        pixels.data[i] = pixels.data[i] + 100; // red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
+    }
+
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+= 4) {
+        pixels.data[i - 150] = pixels.data[i]; // red
+        pixels.data[i + 100] = pixels.data[i + 1]; // green
+        pixels.data[i - 150] = pixels.data[i + 2]; // blue
+    }
+
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        const red = pixels.data[i];
+        const green = pixels.data[i + 1];
+        const blue = pixels.data[i + 2];
+        // const alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // Take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+
+    return pixels;
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
